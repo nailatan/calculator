d55545b834d74ca118e69b8cac7af90a907d2cc7
Natalia <naguado@gmail.com>
2022-05-19 23:04:15 +0200

    Version with class component

--- WithReact/src/Calculator.jsx
+++ WithReact/src/Calculator.jsx
@@ -25,20 +25,60 @@ class Key extends Component {
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = { value: "" };
+    this.state = { operation: "" };
     this.onClick = this.onClick.bind(this);
+    this.operacionesAritmeticas = ["+", "-", "*", "/"];
+    this.equalOperation = ["="];
+    this.clearSigne = ["c"];
+    this.changeSigne = ["+/-"];
+    this.clearLastDigit = ["<="];
   }
 
-  onClick(event) {
-    event.preventDefault;
-    this.setState((prev) => {
-      return { value: prev.value + event.value };
+  someOne = (valores) => (valor) => {
+    return valores.some((item) => {
+      return item === valor;
     });
-  }
+  };
+
+  isClear = (ope) => this.someOne(this.clearSigne)(ope);
+  isOperation = (ope) => this.someOne(this.operacionesAritmeticas)(ope);
+  isEqual = (ope) => this.someOne(this.equalOperation)(ope);
+  isClearLastSigne = (ope) => this.someOne(this.clearLastDigit)(ope);
+  isChangeSigne = (ope) => this.someOne(this.changeSigne)(ope);
+
+  onClick = (event) => {
+    event.preventDefault;
+    console.log(event.value);
+    if (!isNaN(event.value) || this.isOperation(event.value)) {
+      this.setState((prev) => {
+        return { operation: prev.operation + event.value };
+      });
+    } else if (this.isEqual(event.value)) {
+      this.setState((prev) => {
+        return { operation: eval(prev.operation) };
+      });
+    } else if (this.isClear(event.value)) {
+      this.setState((prev) => {
+        return { operation: "" };
+      });
+    } else if (this.isClearLastSigne(event.value)) {
+      this.setState((prev) => {
+        let value = Array.from(prev.operation);
+        value.pop();
+        value.join("");
+        return { operation: value };
+      });
+    } else if (this.isChangeSigne(event.value)) {
+      this.setState((prev) => {
+        let val = `${prev.operation}*-1`;
+        return { operation: eval(val) };
+      });
+    }
+  };
   render() {
     return (
       <div className="Calculadora">
-        <Screen value={this.state.value} />
+        <Screen value={this.state.operation} />
         <div className="botones">
           <p>
             <Key value="c" onKeyClick={this.onClick} />
